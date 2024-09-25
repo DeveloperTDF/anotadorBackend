64f6aed3f62217f5e3f5429ab1091bf34da58f32
tucajas <tucajas@gmail.com>
2024-09-25 13:55:48 -0300

    primer commit con el modelo registrado

---
+++ notas/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Nota (models.Model):
+    fecha   =   models.DateField()
+    usuario =   models.CharField(max_length=15)
+    temario =   models.CharField(max_length=15)
+    titulo  =   models.CharField(max_length=15)
+    apunte  =   models.CharField(max_length=100)
